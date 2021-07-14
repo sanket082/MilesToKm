2ee4b0d875b6a96258df446c859fde0006c79ffd
owner <sanketthakuria12345@gmail.com>
2021-07-13 23:10:57 -0400

    update

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonKmToMiles = (Button) findViewById(R.id.btnkmtomiles);
+        Button buttonMilesToKm = (Button) findViewById(R.id.btnmilestokm);
+        final EditText editTextMiles = (EditText) findViewById(R.id.editTextmiles);
+        final EditText editTextKm = (EditText) findViewById(R.id.editTextkm);
+
+        buttonMilesToKm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Double y = Double.valueOf(editTextMiles.getText().toString());
+                Double x = y/ 0.62;
+                DecimalFormat formatvalue = new DecimalFormat("##.##");
+                editTextKm.setText(formatvalue.format(x));
+            }
+        });
+        buttonKmToMiles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Double x = Double.valueOf(editTextKm.getText().toString());
+                Double y = x * 0.62;
+                DecimalFormat formatvalue = new DecimalFormat("##.##");
+                editTextMiles.setText(formatvalue.format(y));
+            }
+        });
     }
 }
